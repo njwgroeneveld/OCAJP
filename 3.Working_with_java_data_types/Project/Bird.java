9a93a5d4b9267790666c72d3825a30a9d04323b9
njwgroeneveld <njwgroeneveld@gmail.com>
2020-09-27 09:48:30 +0200

    HS2 java basics
    26-9-2020

---
+++ 3.Working_with_java_data_types/Project/Bird.java
@@ -0,0 +1,25 @@
+package birdsfolder;
+
+
+public class Bird{
+	
+	
+	public static int legs;
+	public String name;
+	public double height;
+	public static boolean alive;
+	public Bird thirdBird;
+	
+	
+	public int fly(Integer parameter){
+		//System.out.println("\nammount of legs: " + legs);
+		//System.out.println("name of the bird: " + name);
+		//System.out.println("height of the bird: " + height);
+		//System.out.println("is the bird alive?: " + alive);
+		int param = parameter;
+		return  param;
+		
+	
+	}
+	
+}
---
+++ 3.Working_with_java_data_types/Project/TestClass.java
@@ -0,0 +1,39 @@
+package birdsfolder;
+
+public class TestClass{
+
+public static Bird firstBird;
+
+
+public static void main (String [] args){
+
+	firstBird = new Bird ();
+	
+	
+	Bird secondBird = new Bird();
+	secondBird.legs = 2;
+	secondBird.name = "bigbird";
+	secondBird.alive =  true;
+	secondBird.height = 50.50;
+
+	//firstBird.fly();
+	//secondBird.fly();
+	
+	secondBird.thirdBird = new Bird();
+	//System.out.println(secondBird.fly(10.0f));		// float -> Float -> float -> int
+	//System.out.println(secondBird.fly(new Float(15.0f)));	// Float -> float -> int
+	//secondBird.fly(5.0);				//incompatible type double (64-bit) float (32 bit)
+	secondBird.fly(new Integer(4));	// doesnt compile Integer cannot be converted to Float
+	//secondBird.fly(2);				// doesnt compile integer cannot be converted to Float
+	//secondBird.fly("10");			// doesnt compile string cannot be converted to Float
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+}
